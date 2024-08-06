4a55599c1cdecde005e128eb35c70854bba6b4e2
CMTEQ <mkchadlevi@gmail.com>
2024-08-06 19:52:10 +0200

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -1,9 +1,9 @@
-/*
- * lcd.h
+/*+++++++++++++++++++++++++++++
+ *  lcd2004.h
  *
  *  Created on: Jul 24, 2024
- *      Author: chadl
- */
+ *      Author: CMTEQ
+ **++++++++++++++++++++++++++++*/
 
 #ifndef LCD_H_
 #define LCD_H_
@@ -13,8 +13,9 @@
 #include "stdio.h"
 #include "main.h"
 
+//Chose which LCD you are using
 #define LCD20xN 		// For 20xN LCDs
-//#define LCD16xN			// For 16xN LCDs
+//#define LCD16xN		// For 16xN LCDs
 
 // For row start addresses
 extern const uint8_t ROW_16[];
@@ -34,28 +35,26 @@ extern const uint8_t ROW_20[];
 #define OPT_C	0x02					// Turn on cursor
 #define OPT_B 	0x01					// Turn on cursor blink
 
-#define CURSOR_DISPLAY_SHIFT 0x10		// Move and shift cursor
+#define CURSOR_DISPLAY_SHIFT 0x10		         // Move and shift cursor
 #define OPT_SC 0x08
 #define OPT_RL 0x04
 
 #define FUNCTION_SET 0x20
-#define OPT_DL 0x10						// Set interface data length
-#define OPT_N 0x08						// Set number of display lines
-#define OPT_F 0x04						// Set alternate font
+#define OPT_DL 0x10					 // Set interface data length
+#define OPT_N 0x08					 // Set number of display lines
+#define OPT_F 0x04					 // Set alternate font
 #define SETCGRAM_ADDR 0x040
-#define SET_DDRAM_ADDR 0x80				// Set DDRAM address
+#define SET_DDRAM_ADDR 0x80				 // Set DDRAM address
 
 
 /************************************** Helper macros **************************************/
 #define DELAY(X) HAL_Delay(X)
-
 
 /************************************** LCD defines **************************************/
 #define LCD_NIB 4
 #define LCD_BYTE 8
 #define LCD_DATA_REG 1
 #define LCD_COMMAND_REG 0
-
 
 /************************************** LCD typedefs **************************************/
 #define Lcd_PortType GPIO_TypeDef*
@@ -81,8 +80,7 @@ typedef struct {
 
 } Lcd_HandleTypeDef;
 
-
-/************************************** Public functions **************************************/
+/************************************** Public functions Prototypes **************************************/
 void Lcd_init(Lcd_HandleTypeDef * lcd);
 void Lcd_int(Lcd_HandleTypeDef * lcd, int number);
 void Lcd_string(Lcd_HandleTypeDef * lcd, char * string);
