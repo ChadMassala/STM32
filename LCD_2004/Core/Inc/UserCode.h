d4e6ce91b552d3033cc26eacfe274c9f2c14f5fa
Chad <cmteq1@gmail.com>
2024-12-24 12:10:46 +0200

    Added LCD_20x04

---
+++ LCD_2004/Core/Inc/UserCode.h
@@ -0,0 +1,13 @@
+/*
+ * UserCode.h
+ *
+ *  Created on: Jun 20, 2024
+ *      Author: Powersense
+ */
+
+#ifndef INC_USERCODE_H_
+#define INC_USERCODE_H_
+
+
+
+#endif /* INC_USERCODE_H_ */
---
+++ LCD_2004/Core/Src/UserCode.c
@@ -0,0 +1,16 @@
+/*
+ * UserCode.c
+ *
+ *  Created on: Jun 20, 2024
+ *      Author: Powersense
+ */
+
+#include "main.h"
+
+
+/*void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+   Prevent unused argument(s) compilation warning
+  UNUSED(htim);
+  HAL_GPIO_TogglePin(GPIOD, LED2_Pin);
+}*/
